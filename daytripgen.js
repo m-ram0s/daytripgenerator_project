24c53bc12250d6abfeb5d9f2104d9fde98d91850
Mark Ramos <marco@Marcos-MBP.fios-router.home>
2021-06-21 22:53:11 -0400

    Set logic for alerts and user prompt

--- daytripgen.js
+++ daytripgen.js
@@ -14,3 +14,27 @@ let randomTran = Math.floor(Math.random() * transportation.length);
 
 let randomEnt = Math.floor(Math.random() * entertainment.length);
     console.log(entertainment[randomEnt]);
+
+alert('Welcome to your trip generator. Click OK to find a destination:');
+alert(destination[randomDest]);
+
+alert('Click OK to find a restaurant:');
+alert(restaurant[randomRest]);
+
+alert(' Click OK to find a mode of transportation:');
+alert(transportation[randomTran]);
+
+alert('Click OK to find a form of entertainment:');
+alert(entertainment[randomEnt]);
+
+let userAnswer = prompt('Are you satisfied with the random selections? If you are, enter YES. Enter NO if you would like to re-select your options:');
+    while (userAnswer != 'YES'){
+    }
+    if (userAnswer == 'YES'){
+        alert('Your day trip is complete. Enjoy!')
+        console.log('Your complete trip:', destination[randomDest], '-', restaurant[randomRest], '-', transportation[randomTran], '-', entertainment[randomEnt]);
+    }
+    // else if (userAnswer !== 'YES');{
+    //     console.log('No worries, please select again until you are satisfied!')
+    //     //alert('No worries, please select again until you are satisfied!');
+    // }
